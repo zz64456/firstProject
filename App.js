862742d0d140e43f696a35f1d5dd78bc918dcccd
Neil <z5202dbc@gmail.com>
2020-09-26 09:39:29 +0800

    密碼判斷課程

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
 
-  const [number, setNumber] = useState('')
-  const [phone, setPhone] = useState('')
-  const [result, setResult] = useState('')
-
-  const changePhone = (phone) => {
-    setPhone(phone)
-
-    setResult('猜錯囉！')
-    if (phone === '0933919547') {
-      setResult('你猜對了！！')
+  const [validCode, setValidCode] = useState('')
+  const getNumber = () => {
+    if (validCode == '1234') {
+      return <Text style={{ color: 'yellow', fontSize: 15 }}>輸入正確</Text>
+    } else if (validCode == '') {
+      return <Text style={{ color: 'red', fontSize: 15 }}>請輸入密碼</Text>
+    } else {
+      return <Text style={{ color: 'red', fontSize: 15 }}>輸入錯誤</Text>
     }
   }
 
@@ -21,23 +19,28 @@ export default function App() {
     <View style={styles.container}>
       {/* <Text style={styles.mainText}>Open up App.js to start working on my good app!</Text> */}
       <TextInput
-        style={styles.numberInput}
-        onChangeText={(text) => setNumber(text)}
-        value={number}
-        keyboardType={"numeric"}
-        maxLength={5}
+        maxLength={4}
+        style={{ height: 50, width: 300, borderRadius: 0, borderColor: 'gray', backgroundColor: 'white', color: 'black', fontSize: 28, textAlign: 'center' }}
+        onChangeText={(text) => setValidCode(text)}
+        keyboardType={'numeric'}
+        value={validCode}
+        secureTextEntry={true}
+        autoFocus={true}
       />
-      <Text>你希望帳戶餘額為：{number}</Text>
 
-      <TextInput
-        style={styles.numberInput}
-        onChangeText={(text) => changePhone(text)}
-        value={phone}
-        keyboardType={"numeric"}
-        maxLength={10}
-      />
-      <Text>猜猜看我的手機號碼是多少{phone}</Text>
-      <Text>{result}</Text>
+      {/* {getNumber()} */}
+
+      {/* {validCode === '1234' ? <Text style={{ color: 'yellow', fontSize: 15 }}>輸入正確</Text> : <Text style={{ color: 'red', fontSize: 15 }}>輸入錯誤</Text>} */}
+
+      {validCode.length === 4 ? (
+        validCode === '1234' ? <Text style={{ color: 'yellow', fontSize: 15 }}>輸入正確</Text> : <Text style={{ color: 'red', fontSize: 15 }}>輸入錯誤</Text>
+      ) : <Text style={{ color: 'red', fontSize: 15 }}>請輸入密碼</Text>}
+
+      <TouchableOpacity style={{ backgroundColor: '#00aeef', borderRadius: 20, width: 300, height: 40, justifyContent: 'center', margin: 20 }}>
+        <Text style={{ color: 'white', textAlign: 'center', fontSize: 25 }}>
+          Enter
+        </Text>
+      </TouchableOpacity>
 
       <StatusBar style="auto" />
     </View>
@@ -47,18 +50,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  numberInput: {
-    height: 50,
-    width: 250,
-    backgroundColor: '#dbede4',
-    borderWidth: 3,
-    borderColor: '#319664',
-    color: 'black',
-    textAlign: 'center',
-    fontSize: 25,
-  }
 });
