31279b2830ddc86162216735c7039cce333aefac
Neil <z5202dbc@gmail.com>
2020-09-26 14:44:37 +0800

    做到一半回去看範例

--- App.js
+++ App.js
@@ -2,12 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
+import Submit from './src/components/Button/Submit'
+
 export default function App() {
 
+  const [text, setText] = useState('')
 
   return (
     <View style={styles.container}>
-      <Text style={styles.mainText}>Open up App.js to start working on my good app!</Text>
+      {/* <Text style={styles.mainText}>Open up App.js to start working on my good app!</Text> */}
+      <TextInput
+        onChangeText
+      />
+      <Submit title='' onPress={() => Textsubmit()} />
 
       <StatusBar style="auto" />
     </View>
---
+++ src/components/Button/Submit.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { TouchableOpacity, Text } from 'react-native'
+
+const Submit = props(
+    <TouchableOpacity>
+        <Text>
+            送出
+        </Text>
+    </TouchableOpacity>
+)
